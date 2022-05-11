e6ff4f1eb1c333f21888e7beba987ce97dc61423
DoniaMahmoud <donia1999mahmoud@gmail.com>
2022-05-11 23:31:01 +0200

    3. Active Class Added

--- js/app.js
+++ js/app.js
@@ -22,12 +22,25 @@
  * Define Global Variables
  *
  */
-
+//save all elements with class name section to array
+const sections = document.querySelectorAll("section");
+const arrOfSections = Array.from(sections);
+const navbarSections = document.getElementById("navbar__list");
 /**
  * End Global Variables
  * Start Helper Functions
  *
  */
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
 
 /**
  * End Helper Functions
@@ -36,28 +49,6 @@
  */
 
 // build the nav
-
-// Add class 'active' to section when near top of viewport
-
-// Scroll to anchor ID using scrollTO event
-
-/**
- * End Main Functions
- * Begin Events
- *
- */
-
-// Build menu
-
-// Scroll to section on link click
-
-// Set sections as active
-
-//save all elements with class name sections to array
-const sections = document.querySelectorAll("section");
-const arrOfSections = Array.from(sections);
-const navbarSections = document.getElementById("navbar__list");
-
 function createNavbarSections() {
   arrOfSections.map((element) => {
     let listOfSections = document.createElement("li");
@@ -67,4 +58,35 @@ function createNavbarSections() {
     navbarSections.appendChild(listOfSections);
   });
 }
+// Add class 'active' to section when near top of viewport
+function addActiveClassToSections() {
+  for (sec of sections) {
+    if (isInViewport(sec) === true) {
+      sec.classList.add("active");
+    } else {
+      sec.classList.remove("active");
+    }
+  }
+}
+
+// Scroll to anchor ID using scrollTO event
+
+/**
+ * End Main Functions
+ * Begin Events
+ *
+ */
+window.addEventListener("scroll", function () {
+  addActiveClassToSections();
+});
+// navbarSections.addEventListener("click", function () {
+//   var elmntToView = document.getElementById("section");
+//   elmntToView.scrollIntoView();
+// });
+// Build menu
+
+// Scroll to section on link click
+
+// Set sections as active
+
 createNavbarSections();
